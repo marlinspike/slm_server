539d1bb3ca8c57385a9f12603df167f73c06781f
Reuben Cleetus <rcleetus@gmail.com>
2024-01-07 20:21:38 -0500

    Save_model can save any model filess; app can run several model types

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 import logging
+model_short_name = "Orca-2-7b"
+model_short_name ="TinyLlama-1.1B-Chat-v1.0"
+model_short_name = "phi-2"
+from transformers import pipeline
 
 logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 
@@ -11,22 +16,52 @@ CORS(app)  # Enable CORS
 # Load the Phi-2 model
 #tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 #model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", trust_remote_code=True)
-# Load the Phi-2 model from the local directory
-tokenizer = AutoTokenizer.from_pretrained("./models/phi-2/", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("./models/phi-2/", trust_remote_code=True)
 
-@app.route('/predict', methods=['POST'])
+# Load the Phi-2 model from the local directory
+tokenizer = AutoTokenizer.from_pretrained(f"./models/{model_short_name.lower()}/", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(f"./models/{model_short_name.lower()}/", trust_remote_code=True)
+
+def get_response_from_model(model_name: str, user_request: str, input_data):
+    if(model_name == "phi-2"):
+        input_data = request.json
+        inputs = tokenizer(user_request, return_tensors='pt')
+        outputs = model.generate(inputs['input_ids'], max_length=50)
+        response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        logging.info(f"Response: {response}")
+        return jsonify(response)
+    elif (model_name) == "TinyLlama-1.1B-Chat-v1.0":
+        pipe = pipeline("text-generation", model="TinyLlama-1.1B-Chat-v1.0", device_map="auto")
+        # We use the tokenizer's chat template to format each message - see https://huggingface.co/docs/transformers/main/en/chat_templating
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a friendly chatbot",
+            },
+            {"role": "user", "content": user_request},
+        ]
+        prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+        return(outputs[0]["generated_text"])
+
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
+    input_data = request.json
+    user_request = input_data['text']
     logging.info(f"Running predict() with request: {request.json}")
+    return get_response_from_model(model_short_name, user_request, input_data)
+
+    """
     input_data = request.json
     inputs = tokenizer(input_data['text'], return_tensors='pt')
     outputs = model.generate(inputs['input_ids'], max_length=50)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    logging.info(f"Response: {response}")
     return jsonify(response)
+    """
 
 @app.route('/test', methods=['POST'])
 def test():
     return jsonify("Hello World")
 
 if __name__ == '__main__':
-    app.run(debug=True, port="6001")
+    app.run(debug=False, port="6001")
--- save_model.py
+++ save_model.py
@@ -1,12 +1,31 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from utils import ensure_path_exists
 
+# Some models to try:
+# phi-2
+# Orca-2-7b
 # Define the model name
-model_name = "microsoft/phi-2"
+model_short_name = "Orca-2-7b"
+model_vendor_name = "microsoft"
+
+model_short_name ="TinyLlama-1.1B-Chat-v1.0"
+model_vendor_name = "TinyLlama"
+
+model_short_name = "phi-2"
+model_vendor_name = "microsoft"
+
+model_name = f"{model_vendor_name}/{model_short_name}"
+
+
+tokenizer = None
+model = None
 
 # Download and cache the model and tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
 
+ensure_path_exists(f"./models/{model_short_name.lower()}")
 # Save to the specified directory
-model.save_pretrained('./models/phi-2')
-tokenizer.save_pretrained('./models/phi-2')
+model.save_pretrained(f"./models/{model_short_name.lower()}")
+tokenizer.save_pretrained(f"./models/{model_short_name.lower()}")
+
